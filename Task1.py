261cb0d36e096d374aa1cbcea9b2f5d0bc9b0376
i-m-dremov <imdremov@gmail.com>
2024-10-23 17:30:43 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+# 1st programm
+print(9 ** 0.5 * 5)
+
+#2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd programm
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+#4th programm
+x = ('123.456')
+y = float(x)
+print(int(y * 10 % 10))

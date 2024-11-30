703b8e02f874637be9b95284de6588b530b2194a
i-m-dremov <imdremov@gmail.com>
2024-11-30 15:10:48 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,9 @@
+
+def test_function():
+	def inner_function():
+		print('Я в области видимости функции test_function')
+	inner_function()
+test_function()
+inner_function() 	# NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+					# Невозможно прочитать функцию в глобальном пространстве,
+					# так как она находиться внутри другой функции

f0fbb0d1c64692e338c98f5da1fdc467d89730a2
i-m-dremov <imdremov@gmail.com>
2024-12-07 14:18:52 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,81 @@
+#Задача "Нужно больше этажей":
+
+class House:
+
+	def __init__(self, name, number_of_floors):
+		self.name = name
+		self.number_of_floors = number_of_floors
+
+	def goto(self, new_floor):
+		if new_floor > self.number_of_floors:
+			print(f'Такого этажа в {self.name} не существует')
+		else:
+			for i in range(new_floor):
+				print(f' Этаж {i + 1} в {self.name}')
+
+	def __str__(self):
+		return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+	def __len__(self):
+		return self.number_of_floors
+
+	def __eq__(self, other):
+		if not isinstance(other, House):
+			return print('Объекты не соответствуют типу House')
+		return self.number_of_floors == other.number_of_floors
+
+	def __lt__(self, other):
+		if not isinstance(other, House):
+			return print('Объекты не соответствуют типу House')
+		return self.number_of_floors < other.number_of_floors
+
+	def __le__(self, other):
+		if not isinstance(other, House):
+			return print('Объекты не соответствуют типу House')
+		return self.number_of_floors <= other.number_of_floors
+
+	def __gt__(self, other):
+		if not isinstance(other, House):
+			return print('Объекты не соответствуют типу House')
+		return self.number_of_floors > other.number_of_floors
+
+	def __ge__(self, other):
+		if not isinstance(other, House):
+			return print('Объекты не соответствуют типу House')
+		return self.number_of_floors >= other.number_of_floors
+
+	def __ne__(self, other):
+		if not isinstance(other, House):
+			return print('Объекты не соответствуют типу House')
+		return self.number_of_floors != other.number_of_floors
+
+	def __add__(self, value):
+		if not isinstance(value, int):
+			return print('Новый операнд должен быть типом int')
+		else:
+			self.number_of_floors += value
+			return self
+
+	def __iadd__(self, other):
+		return self.__add__(other)
+
+	def __radd__(self, other):
+		return self.__add__(other)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(h1 == h2) # __eq__
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+h1 += 10 # __iadd__
+print(h1)
+h2 = 10 + h2 # __radd__
+print(h2)
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__

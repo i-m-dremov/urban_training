2edf553aab9bf95c09d7f346ae8197819b9369f9
i-m-dremov <imdremov@gmail.com>
2024-12-07 08:11:54 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,32 @@
+#Задача "Магические здания"
+
+class House:
+
+	def __init__(self, name, number_of_floors):
+		self.name = name
+		self.number_of_floors = number_of_floors
+
+	def goto(self, new_floor):
+		if new_floor > self.number_of_floors:
+			print(f'Такого этажа в {self.name} не существует')
+		else:
+			for i in range(new_floor):
+				print(f' Этаж {i + 1} в {self.name}')
+
+	def __str__(self):
+		return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+	def __len__(self):
+		return self.number_of_floors
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+#__str__
+print(h1)
+print(h2)
+
+#__len__
+print(len(h1))
+print(len(h2))
